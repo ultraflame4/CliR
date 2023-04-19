707abc6c06846ad3990dabf635d8078afc93a898
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-19 23:40:45 +0800

    updated test

--- test.py
+++ test.py
@@ -3,5 +3,7 @@ from PIL import Image
 import CliR
 
 image = Image.open("./resources/pexels-photo-302769.jpeg")
+image2 = Image.open("./resources/pexels-pixabay-206359.jpg")
 
-CliR.render(image,(100,50))
+CliR.render(image,(170,60))
+CliR.render(image2,(170,60))

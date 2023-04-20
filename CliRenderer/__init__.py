19fa84983f5ed96ae04250c147ff0ffb7e31c11a
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-20 14:57:40 +0800

    added partial video support and fixed rgba problems

--- CliRenderer/__init__.py
+++ CliRenderer/__init__.py
@@ -8,19 +8,29 @@ from CliRenderer.colorer import color_twotone, color_char
 from CliRenderer.core import Flags
 
 
-def render(source_: Image.Image, out_size=(170, 50),bg_intensity=1)->Text:
+def render(source_: Image.Image, out_size=(170, 50),bg_intensity=1,skip_resize=False)->Text:
     """
     Renders a single image into unicode text.
 
     :param source_: The source image
     :param out_size: The size of the final text in the form of (columns, lines).
     :return: returns a rich Text object to be printed to the console with rich
+    :param skip_resize: If set to true, the image will not be resized. This is useful if the image is already the correct size.
     """
 
     FinalImageSize = (PixelsPerChar[0] * out_size[0], PixelsPerChar[1] * out_size[1])
 
-    image = source_.resize(FinalImageSize)
-    gray = source_.resize(FinalImageSize).convert("L")
+
+    if not skip_resize:
+        image = source_.resize(FinalImageSize).convert("RGB")
+        gray = source_.resize(FinalImageSize).convert("L")
+    else:
+        image = source_.convert("RGB")
+        gray = source_.convert("L")
+
+    if image.size != FinalImageSize:
+        raise ValueError("The image size does not match the output size. Cannot skip resize!")
+
 
     if Flags.DEBUG:
         os.makedirs("./build", exist_ok=True)
@@ -39,3 +49,6 @@ def render(source_: Image.Image, out_size=(170, 50),bg_intensity=1)->Text:
 
     string = color_char(image, chars, mask, bg_intensity)
     return string
+
+def render_video():
+    pass
--- CliRenderer/__main__.py
+++ CliRenderer/__main__.py
@@ -3,14 +3,17 @@ from pathlib import Path
 import typer as typer
 from PIL import Image
 from rich.console import Console
+import imageio as iio
+from rich.progress import track
 
 from CliRenderer import render, Flags
 
 
-def cli_main(source: Path = typer.Argument(...,help="The source image"),
+def cli_main(source: Path = typer.Argument(...,help="The path to source image."),
          width: int = typer.Option(150, "--width", "-w", min=1, help="The width of the output in no. of characters."),
          height: int = typer.Option(50, "--height", "-h", min=1, help="The height of the output in no. of lines"),
          autosize: bool = typer.Option(False, help="Automatically sets the output size to fit the terminal. Overrides --width and --height. Only works on supported terminals."),
+         video: bool = typer.Option(False, "--video", "-v", help="Set this flag if source is a video."),
          debug: bool = typer.Option(False, help="Enables debug mode. This will save the intermediate images to the build folder."),
          bg_intensity: float = typer.Option(1.0,"--bg-intensity","-bgi", min=0.0, max=1.0, help="The intensity of the background. The closer to 0 the darker the background.")
          ):
@@ -19,13 +22,25 @@ def cli_main(source: Path = typer.Argument(...,help="The source image"),
     """
 
     Flags.DEBUG = debug
+    console = Console()
+
     if autosize:
         import shutil
         width, height = shutil.get_terminal_size()
 
-    source_image = Image.open(source)
+    if video:
+        texts = []
+        for frame in track(iio.imiter(source, plugin="pyav"), description="Processing..."):
+            source_image = Image.fromarray(frame)
+            text = render(source_image, (width, height), bg_intensity)
+            texts.append(text)
+        for text in texts:
+            console.print(text)
+        return
+
+    source_image = Image.fromarray(iio.v3.imread(source))
     text = render(source_image, (width, height), bg_intensity)
-    console = Console()
+
     console.print(text)
 
 

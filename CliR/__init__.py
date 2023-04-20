a1dca33a68358cd0a7ebe2a5ded4d8b2d7673dd1
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-20 10:55:43 +0800

    render now returns a Text object

--- CliR/__init__.py
+++ CliR/__init__.py
@@ -2,19 +2,20 @@ import os
 
 from PIL import Image
 from rich.console import Console
+from rich.text import Text
 
 from CliR.chartools import PixelsPerChar, split_to_char, pixels2Char
 from CliR.colorer import color_twotone, color_char
 from CliR.core import Flags
 
 
-def render(source_: Image.Image, out_size=(500, 500)):
+def render(source_: Image.Image, out_size=(500, 500))->Text:
     """
     Renders a single image into unicode text.
 
     :param source_: The source image
     :param out_size: The size of the final text in the form of (columns, lines).
-    :return:
+    :return: returns a rich Text object to be printed to the console with rich
     """
 
     FinalImageSize = (PixelsPerChar[0] * out_size[0], PixelsPerChar[1] * out_size[1])
@@ -36,7 +37,6 @@ def render(source_: Image.Image, out_size=(500, 500)):
             char_row += pixels2Char(cell)
         chars.append(char_row)
 
-    console = Console()
-    string = color_char(image, chars, mask, 1)
 
-    console.print(string)
+    string = color_char(image, chars, mask, 1)
+    return string

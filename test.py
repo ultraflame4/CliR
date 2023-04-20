4357ff222ac7e7e3117b2c0a152c626480b9acd3
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-20 11:02:35 +0800

    updated test file

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 from PIL import Image
+from rich.console import Console
 
 import CliR
 
 image = Image.open("./resources/pexels-photo-302769.jpeg")
 image2 = Image.open("./resources/pexels-pixabay-206359.jpg")
 
-CliR.render(image,(100,40))
-# CliR.render(image2,(170,60))
+console = Console()
+txt = CliR.render(image,(170,60))
+txt2 = CliR.render(image2,(170,60))
+console.print(txt)
+console.print(txt2)

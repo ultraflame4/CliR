3f48c90a600281cff5f6851e016aa483e8394177
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-01 17:35:16 +0800

    fixed init

--- test/init.py
+++ test/init.py
@@ -2,7 +2,7 @@
 Import this file to add the parent directory to the path and setup debug stuff.
 """
 import sys
-from CliRenderer.core import Flags
-
 sys.path.append('..')
+
+from CliRenderer.core import Flags
 Flags.DEBUG=True

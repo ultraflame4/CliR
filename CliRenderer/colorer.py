274a631a9ed0dcbb533b130f2149ff4f46321771
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-01 18:36:57 +0800

    added blending to the fore and back colors

--- CliRenderer/colorer.py
+++ CliRenderer/colorer.py
@@ -24,12 +24,19 @@ def color_char(chars: list[str], charcolors: np.ndarray, bg_intensity: float = 1
     """
 
     string = Text()
-    print(charcolors.shape,len(chars))
+    print(charcolors.shape, len(chars))
     for y, row in enumerate(chars):
         for x, char in enumerate(row):
+            rawfore_ = charcolors[y, x, 0]
+            rawback_ = charcolors[y, x, 1]
 
-            fore_ = charcolors[y, x, 0]
-            back_ = charcolors[y, x, 1]
+
+            # Blend the back and fore colors in a 80:20 ratio so that the background color is effectively the 20% th percentile of cell colors
+            # TLDR: The background color is the darkest color in the cell,
+            # by blending it with the foreground color it looks nicer and is more representative of the cell color
+            # The same goes for the foreground color (which is the brightest), but it is blended with the background color instead
+            fore_ = rawfore_*0.8 + rawback_*0.2
+            back_ = rawfore_*0.2 + rawback_*0.8
 
             fore = Color.from_rgb(fore_[0], fore_[1], fore_[2])
             back = Color.from_rgb(back_[0] * bg_intensity, back_[1] * bg_intensity, back_[2] * bg_intensity)

ed86ceb6b2ab74cea576d80c5b1d0c29f0ab65d4
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-06-01 18:44:22 +0800

    added speed test

---
+++ test/test-speed.py
@@ -0,0 +1,28 @@
+import init
+from pathlib import Path
+import time
+
+from PIL import Image
+from rich.console import Console
+
+import CliRenderer
+
+
+CliRenderer.Flags.DEBUG=False
+Test_Count = 5
+
+image2 = Image.open("../resources/pexels-pixabay-206359.jpg")
+console = Console()
+
+timings = []
+
+for i in range(Test_Count):
+    start_time = time.time()
+    txt2 = CliRenderer.render(image2)
+    timings.append((time.time()-start_time)*1000)
+
+print(f"Average timings: {sum(timings)/len(timings)} ms")
+print(f"Biggest: {max(timings)} ms")
+print(f"Smallest: {min(timings)} ms")
+
+

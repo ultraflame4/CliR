c1c960f0c2dd370b169a5e81a9434ecc6da80c4c
ultr42 <ultr42@skiff.com>
2023-11-08 22:07:45 +0800

    Updated speed test

--- test/test-speed.py
+++ test/test-speed.py
@@ -1,4 +1,5 @@
 # import init
+import statistics
 from pathlib import Path
 import time
 
@@ -17,22 +18,32 @@ console = Console()
 
 timings = []
 
+TEST_KWARGS = {
+    "out_size": (170, 50),
+    "bg_intensity": 1
+}
+
+print("Starting speed test with KWARGS", TEST_KWARGS)
+
 for i in range(Test_Count):
     start_time = time.time()
-    txt = CliRenderer.render(image)
+    txt = CliRenderer.render(image, **TEST_KWARGS)
     resultA = (time.time()-start_time)*1000
     print(f"Test {i} A: {resultA} ms")
 
     start_time = time.time()
-    txt2 = CliRenderer.render(image2)
+    txt2 = CliRenderer.render(image2, **TEST_KWARGS)
     resultB = (time.time() - start_time) * 1000
     print(f"Test {i} B: {resultB} ms")
 
     timings.append(resultA)
     timings.append(resultB)
 
-print(f"Average timings: {sum(timings)/len(timings)} ms")
-print(f"Max time: {max(timings)} ms")
+print("--------------------\nTest KWARGS", TEST_KWARGS)
+print("--Test timings--")
+print(f"Avg: {sum(timings)/len(timings)} ms")
+print(f"Med: {statistics.median(timings)} ms")
+print(f"Max: {max(timings)} ms")
 print(f"Min: {min(timings)} ms")
 
 
